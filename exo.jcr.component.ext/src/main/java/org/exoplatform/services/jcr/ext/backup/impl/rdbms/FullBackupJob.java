27b173b7f87c4b9176125e084ff8edf7735dc7ff
aboughzela <aboughzela@exoplatform.com>
2015-01-20 11:25:01 +0100

    JCR-2310 : Update TokuMX documentation (Apply given remarks)

--- exo.jcr.component.ext/src/main/java/org/exoplatform/services/jcr/ext/backup/impl/rdbms/FullBackupJob.java
+++ exo.jcr.component.ext/src/main/java/org/exoplatform/services/jcr/ext/backup/impl/rdbms/FullBackupJob.java
@@ -18,127 +18,13 @@
  */
 package org.exoplatform.services.jcr.ext.backup.impl.rdbms;
 
-import org.exoplatform.commons.utils.PrivilegedFileHelper;
-import org.exoplatform.services.jcr.core.ManageableRepository;
-import org.exoplatform.services.jcr.ext.backup.BackupConfig;
-import org.exoplatform.services.jcr.ext.backup.impl.AbstractFullBackupJob;
-import org.exoplatform.services.jcr.ext.backup.impl.FileNameProducer;
-import org.exoplatform.services.jcr.impl.backup.BackupException;
-import org.exoplatform.services.jcr.impl.backup.Backupable;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.List;
-
-import javax.jcr.RepositoryException;
 
 /**
  * Created by The eXo Platform SARL Author : Alex Reshetnyak alex.reshetnyak@exoplatform.com.ua Nov
  * 21, 2007
  */
-public class FullBackupJob extends AbstractFullBackupJob
+public class FullBackupJob extends org.exoplatform.services.jcr.ext.backup.impl.FullBackupJob
 {
-   /**
-   * Logger.
-   */
-   protected static final Log LOG = ExoLogger.getLogger("exo.jcr.component.ext.FullBackupJob");
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   protected URL createStorage() throws FileNotFoundException, IOException
-   {
-      FileNameProducer fnp =
-         new FileNameProducer(config.getRepository(), config.getWorkspace(),
-            PrivilegedFileHelper.getAbsolutePath(config.getBackupDir()), super.timeStamp, true, true);
-
-      return new URL("file:" + PrivilegedFileHelper.getAbsolutePath(fnp.getNextFile()));
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   public void init(ManageableRepository repository, String workspaceName, BackupConfig config, Calendar timeStamp)
-   {
-      this.repository = repository;
-      this.workspaceName = workspaceName;
-      this.config = config;
-      this.timeStamp = timeStamp;
-
-      try
-      {
-         url = createStorage();
-      }
-      catch (FileNotFoundException e)
-      {
-         LOG.error("Full backup initialization failed ", e);
-         notifyError("Full backup initialization failed ", e);
-      }
-      catch (IOException e)
-      {
-         LOG.error("Full backup initialization failed ", e);
-         notifyError("Full backup initialization failed ", e);
-      }
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   public void run()
-   {
-      notifyListeners();
-
-      try
-      {
-         repository.getWorkspaceContainer(workspaceName).setState(ManageableRepository.SUSPENDED);
-
-         List<Backupable> backupableComponents =
-            repository.getWorkspaceContainer(workspaceName).getComponentInstancesOfType(Backupable.class);
-
-         // backup all components
-         for (Backupable component : backupableComponents)
-         {
-            component.backup(new File(getStorageURL().getFile()));
-         }
-      }
-      catch (BackupException e)
-      {
-         LOG.error("Full backup failed " + getStorageURL().getPath(), e);
-         notifyError("Full backup failed", e);
-      }
-      catch (RepositoryException e)
-      {
-         LOG.error("Full backup failed " + getStorageURL().getPath(), e);
-         notifyError("Full backup failed", e);
-      }
-      finally
-      {
-         try
-         {
-            repository.getWorkspaceContainer(workspaceName).setState(ManageableRepository.ONLINE);
-         }
-         catch (RepositoryException e)
-         {
-            LOG.error("Full backup failed " + getStorageURL().getPath(), e);
-            notifyError("Full backup failed", e);
-         }
-      }
-
-      state = FINISHED;
-      notifyListeners();
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   public void stop()
-   {
-      LOG.info("Stop requested " + getStorageURL().getPath());
-   }
 }
